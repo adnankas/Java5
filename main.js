67963657787c312e15da2c84a52bead1aa145787
Adnan Kasumaj <ak45732@ubt-uni.net>
2020-05-02 22:00:52 +0200

    commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+
+function register(){
+    x.style.left = "-550px";
+    y.style.left = "-750px";
+ }
+ function login(){
+    x.style.left = "3px";
+    y.style.left = "410px";
+ }
